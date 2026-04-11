11492a3dc3bc7f02eb38c0b6016eb9931f934770
Ahmed_Mohamed <e01018478430@gmail.com>
2026-04-11 20:42:47 +0200

    task

--- Back-End/restaurant_Management_System/src/main/java/restaurant_management_system/config/swaggerConfig/SwaggerConfig.java
+++ Back-End/restaurant_Management_System/src/main/java/restaurant_management_system/config/swaggerConfig/SwaggerConfig.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.Configuration;
                         contact =
                         @Contact(
                                 name = "Ahmed Mohamed",
-                                 email = "e01018478430@gmail.com" ,
+                                 email = "ahmed.mohamed.swe1@gmail.com" ,
                                  url = "https://www.linkedin.com/in/ahmedmohammed-jv/"
                                 ),
                         license =
